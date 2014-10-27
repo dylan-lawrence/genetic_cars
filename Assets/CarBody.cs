61871510d72147378e5f48728f59bf46259df5ca
Dylan Lawrence <dlawre14@slu.edu>
2014-10-27 00:31:32 -0500

    more work, linux seems to dislike some of the rendering

--- Assets/CarBody.cs
+++ Assets/CarBody.cs
@@ -4,45 +4,32 @@ using Vectrosity;
 
 public class CarBody : MonoBehaviour {
 
+	VectorLine myline;
+
 	// Use this for initialization
 	void Start () {
-		Vector2[] p = new Vector2[240];
-		VectorLine body = new VectorLine ("CarBody", p, null, 2.0f, LineType.Continuous, Joins.Weld);
-		
-		Vector2[] points = new Vector2[] {
-			new Vector2(Random.Range(-1.0f,-.66f),Random.Range (0.0f,1.0f)) * 100,
-			new Vector2(Random.Range(-.66f,-.33f),Random.Range (0.0f,1.0f)) * 100,
-			new Vector2(Random.Range(-.33f,0.0f),Random.Range (0.0f,1.0f)) * 100,
-			new Vector2(Random.Range(0.0f,.33f),Random.Range (0.0f,1.0f)) * 100,
-			new Vector2(Random.Range(.33f, .66f),Random.Range (0.0f, 1.0f)) * 100,
-			new Vector2(Random.Range(.66f,1.0f),Random.Range (0.0f,1.0f)) * 100,
-			new Vector2(Random.Range(.66f,1.0f),Random.Range (-1.0f,0.0f)) * 100,
-			new Vector2(Random.Range(.33f,.66f),Random.Range (-1.0f,0.0f)) * 100,
-			new Vector2(Random.Range(0.0f,.33f),Random.Range (-1.0f,0.0f)) * 100,
-			new Vector2(Random.Range(-.33f,0.0f),Random.Range (-1.0f,0.0f)) * 100,
-			new Vector2(Random.Range(-.66f,-.33f),Random.Range (-1.0f,0.0f)) * 100,
-			new Vector2(Random.Range(-1.0f,-.66f),Random.Range (-1.0f,0.0f)) * 100				
-		};
-		
-		body.MakeSpline (points, 239, true);
-		
-		body.color = Color.red;
-		body.Draw();
-		
-		Vector2[] p2 = new Vector2[100];
-		VectorLine wheel_one = new VectorLine("WheelOne", p2, null, 2.0f, LineType.Continuous, Joins.Weld);
-		
-		wheel_one.MakeCircle(points[0], Random.Range (0.1f,1.0f) * 50);
-		wheel_one.SetColor (Color.blue);
-		wheel_one.Draw ();
-		
-		VectorPoints center = new VectorPoints("Hub", points, null, 3.0f);
-		center.SetColor(Color.yellow);
-		center.Draw ();
+		Vector2[] points = GetComponent<PolygonCollider2D> ().points;
+		Vector3[] points3 = new Vector3[points.Length];
+
+		for (int i = 0; i<points.Length; i++)
+			points3 [i] = (Vector3)points [i];
+
+		Vector3[] p = new Vector3[points.Length + 1];
+		myline = new VectorLine ("CarBody", p, null, 2.0f, LineType.Continuous, Joins.Weld);
+
+		myline.MakeSpline (points3, points.Length, true);
+
+		myline.SetColor (Color.yellow);
+		myline.drawTransform = transform;
+		myline.Draw3D ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+
+	}
+
+	void LateUpdate() {
+		myline.Draw3D ();
 	}
 }
--- Assets/FloorMake.cs
+++ Assets/FloorMake.cs
@@ -1,58 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Vectrosity;
-using System.Security.Cryptography;
 
 public class FloorMake : MonoBehaviour {
-	
-	private static readonly RNGCryptoServiceProvider _generator = new RNGCryptoServiceProvider();
-	
-	public static float Between(float minimumValue, float maximumValue)
-	{
-		byte[] randomNumber = new byte[1];
-		
-		_generator.GetBytes(randomNumber);
-		
-		float asciiValueOfRandomCharacter = (float)System.Convert.ToDouble(randomNumber[0]);
-		
-		// We are using Math.Max, and substracting 0.00000000001, 
-		// to ensure "multiplier" will always be between 0.0 and .99999999999
-		// Otherwise, it's possible for it to be "1", which causes problems in our rounding.
-		float multiplier = Mathf.Max(0, (asciiValueOfRandomCharacter / 255f) - 0.00000000001f);
-		
-		// We need to add one to the range, to allow for the rounding done with Math.Floor
-		float range = maximumValue - minimumValue + 1;
-		
-		float randomValueInRange = Mathf.Floor(multiplier * range);
-		
-		return (minimumValue + randomValueInRange);
-	}
-	
+
+	public float difficulty;
+
 	// Use this for initialization
 	void Start () {
 		Vector3[] p = new Vector3[10000];
 		VectorLine myline = new VectorLine("Floor", p, null, 5.0f, LineType.Continuous, Joins.Weld);
-		
+
 		Vector3[] points = new Vector3[500];
 		Vector3 prev = Vector3.zero;
 		for (int i = 0; i<500; i++) {
 		  if (i < 50)
 		  	prev += new Vector3(1, 0, 0);
 		  else
-		    prev += new Vector3(1, Between(-1.0f, 1.0f), 0);
-		    
+		    prev += new Vector3(1, Random.Range(-difficulty, difficulty), 0);
+
 		  points[i] = prev;
 		}
-		
+
 		myline.MakeSpline(points);
 		myline.SetColor (Color.red);
-		
+
 		myline.collider = true;
 		myline.Draw3D ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
