bb9f1aed0ceff56c9fe6460ed98e25ba8829829c
Dylan Lawrence <dlawre14@slu.edu>
2014-10-26 22:01:38 -0500

    rework of entire structure of project, some progress has been made

--- Assets/CarBody.cs
+++ Assets/CarBody.cs
@@ -1,11 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Vectrosity;
 
 public class CarBody : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+		Vector2[] p = new Vector2[240];
+		VectorLine body = new VectorLine ("CarBody", p, null, 2.0f, LineType.Continuous, Joins.Weld);
+		
+		Vector2[] points = new Vector2[] {
+			new Vector2(Random.Range(-1.0f,-.66f),Random.Range (0.0f,1.0f)) * 100,
+			new Vector2(Random.Range(-.66f,-.33f),Random.Range (0.0f,1.0f)) * 100,
+			new Vector2(Random.Range(-.33f,0.0f),Random.Range (0.0f,1.0f)) * 100,
+			new Vector2(Random.Range(0.0f,.33f),Random.Range (0.0f,1.0f)) * 100,
+			new Vector2(Random.Range(.33f, .66f),Random.Range (0.0f, 1.0f)) * 100,
+			new Vector2(Random.Range(.66f,1.0f),Random.Range (0.0f,1.0f)) * 100,
+			new Vector2(Random.Range(.66f,1.0f),Random.Range (-1.0f,0.0f)) * 100,
+			new Vector2(Random.Range(.33f,.66f),Random.Range (-1.0f,0.0f)) * 100,
+			new Vector2(Random.Range(0.0f,.33f),Random.Range (-1.0f,0.0f)) * 100,
+			new Vector2(Random.Range(-.33f,0.0f),Random.Range (-1.0f,0.0f)) * 100,
+			new Vector2(Random.Range(-.66f,-.33f),Random.Range (-1.0f,0.0f)) * 100,
+			new Vector2(Random.Range(-1.0f,-.66f),Random.Range (-1.0f,0.0f)) * 100				
+		};
+		
+		body.MakeSpline (points, 239, true);
+		
+		body.color = Color.red;
+		body.Draw();
+		
+		Vector2[] p2 = new Vector2[100];
+		VectorLine wheel_one = new VectorLine("WheelOne", p2, null, 2.0f, LineType.Continuous, Joins.Weld);
+		
+		wheel_one.MakeCircle(points[0], Random.Range (0.1f,1.0f) * 50);
+		wheel_one.SetColor (Color.blue);
+		wheel_one.Draw ();
+		
+		VectorPoints center = new VectorPoints("Hub", points, null, 3.0f);
+		center.SetColor(Color.yellow);
+		center.Draw ();
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Follow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+	
+	public GameObject target;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = target.transform.position - new Vector3(0,0,5);
+	}
+}
