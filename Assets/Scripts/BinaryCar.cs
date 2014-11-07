676756e3f01982bf66ab74a567ec44ec9cfae329
Dylan Lawrence <dlawre14@slu.edu>
2014-11-07 09:33:59 -0600

    more work on redoing genome

---
+++ Assets/Scripts/BinaryCar.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vectrosity;
+
+public class BinaryCar : MonoBehaviour {
+
+	public int no_points;
+
+	private VectorLine body;
+
+	public void BuildCar(BinaryGenome genome) {
+		//assume the car has 5 points in its body, first 5 genes
+		#region car_body
+		Vector2[] points = new Vector2[no_points];
+
+		for (int i = 0; i < no_points; i++) {
+			points[i] = new Vector2 (System.Convert.ToInt32(genome[i].gene.Substring(0,4),2),System.Convert.ToInt32(genome[i].gene.Substring(4,4),2));
+		}
+
+		points = Statics.CircleSort (points);
+
+		GetComponent<PolygonCollider2D>().points = points;
+
+		Vector3[] p = new Vector3[no_points + 1];
+
+		for (int i = 0; i < no_points; i++)
+			p [i] = (Vector3) points [i];
+
+		p [no_points] = (Vector3)points [0];
+
+		body = new VectorLine ("CarBody", p, null, 3.0f, LineType.Continuous, Joins.Fill);
+
+		Color[] cols = new Color[no_points];
+
+		for (int i = 0; i < no_points; i++)
+			cols [i] = genome [i].color;
+
+		body.smoothColor = true;
+		body.SetColors (cols);
+		body.collider = true;
+
+		body.Draw3DAuto ();
+		#endregion
+
+		#region car_wheels
+
+
+
+		#endregion
+	}
+
+	public void CarDestroy() {
+		//Handles removing lines
+		VectorLine.Destroy (ref body);
+
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/Monitor.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Monitor : MonoBehaviour {
+
+	//This collects and gathers all information going on
+	//It also collects and handles genomes in the running
+	//and handles crossovers or other combinations
+
+	public GameObject CarObj;
+
+	// Use this for initialization
+	void Start () {
+		List<BinaryGenome> genomes = new List<BinaryGenome> ();
+		genomes.Add (new BinaryGenome (5, 8, Color.red));
+		genomes.Add (new BinaryGenome (5, 8, Color.yellow));
+		genomes.Add (new BinaryGenome (5, 8, Color.blue));
+		genomes.Add (new BinaryGenome (5, 8, Color.green));
+		genomes.Add (new BinaryGenome (5, 8, Color.black));
+
+		for (int i = 0; i<10; i++) {
+			GenomeTester.Shuffle(genomes);
+			genomes[0].RandomCrossover(genomes[1]);
+		}
+
+		Debug.Log (genomes[0]);
+
+		GameObject obj = (GameObject) Instantiate (CarObj, new Vector3 (2, 2, 0), Quaternion.identity);
+		obj.GetComponent<BinaryCar> ().BuildCar (genomes[0]);
+
+		StartCoroutine(DestroyCar(obj.GetComponent<BinaryCar>()));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator DestroyCar (BinaryCar car) {
+		yield return new WaitForSeconds(3);
+		car.CarDestroy ();
+	}
+}
